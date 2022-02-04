ce613f8b7d3b0e06203fb43d65eb563e0112edc4
Shibi Balamurugan <shibibala1@gmail.com>
2022-02-04 03:04:20 -0500

    Add update user endpoint

--- users/usersController.js
+++ users/usersController.js
@@ -1,5 +1,6 @@
 const mongoDBConnection = require('../mongoDBConnection');
 const {generateAccessToken, authenticateToken} = require('../helpers/jwt');
+const {ObjectId} = require('mongodb');
 
 /*
 POST \register
@@ -18,7 +19,7 @@ ReqBody:
 }
 
 Response:
-[_id string]
+{_id, token}
 */
 module.exports.register = (req, res, next) => {
   // Validate unique email
@@ -40,19 +41,38 @@ module.exports.register = (req, res, next) => {
 }
 
 
+/*
+POST \update
 
+ReqBody: 
+{
+  "bio": {
+    "first": "string",
+    "last": "string",
+    "orgName": "string"
+  }
+}
+
+Response:
+{message: "string"}
+*/
 module.exports.updateUserProfile = (req, res) => {
-
-  mongoDBConnection.get().collection('LanternUsers').find({"auth.email":req.body.auth.email}).toArray((e,docs) => {
-    if (docs.length != 0) {
-      return res.status(400).json({message:"Email already in use"});
+  mongoDBConnection.get().collection('LanternUsers').find({"auth.email":req.user.email}).toArray((e,docs) => {
+    if (docs.length == 0) {
+      return res.status(400).json({message:"Account invalid"});
     } else {
-      // Insert document
-      mongoDBConnection.get().collection('LanternUsers').insertOne(req.body, (e, dbRes) => {
+      // Update document
+      const modifiedBio = {
+        "bio.first": req.body.bio.first,
+        "bio.last": req.body.bio.last,
+        "bio.orgName": req.body.bio.orgName
+      }
+
+      mongoDBConnection.get().collection('LanternUsers').updateOne({_id: docs[0]._id}, {$set:modifiedBio}, (e, dbRes) => {
         if (e) {
           return res.status(500).json({message:"Database Update Error"});
         } else {
-          return res.status(201).json();
+          return res.status(200).json({message:"Database Updated"});
         }
       });
     }
