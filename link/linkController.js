d7a760a204497c3a43f2a71ceb358084ffcbfd51
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-02-08 16:48:57 -0500

    #15 next step link up get token where applicable

--- link/linkController.js
+++ link/linkController.js
@@ -1,5 +1,18 @@
 const plaid = require('../plaidConnection');
 const mongoDBConnection = require("../mongoDBConnection");
+const jwt = require("jsonwebtoken");
+
+module.exports.getAccessTokens = (req, res, next) => {
+  // Get all of user's access tokens
+  mongoDBConnection.get().collection("LanternUsers").find({ 'auth.email': req.user.email }, {accessTokens: 1}).toArray((e, docs) => {
+    if (e) {
+      res.status(403).json({ message: "No valid Items (with access tokens) found for user!" });
+    } else {
+      req.user.accessTokens = docs;
+      next();
+    }
+  });
+}
 
 /*
 GET /create
@@ -38,10 +51,10 @@ module.exports.exchange = async (req, res) => {
   try {
     const response = await plaid.client.itemPublicTokenExchange(exchangeRequest);
     const accessToken = response.data.access_token;
-    //const itemId = response.data.item_id;
-    // Store full item in database
+    //const itemId = response.data.item_id
+    // Store full item and accessTokens in database
     let item = await plaid.getItem(accessToken);
-    mongoDBConnection.get().collection('LanternUsers').updateOne({ 'auth.email': req.user.email }, { $push: {items: item} }, (e, dbRes) => {
+    mongoDBConnection.get().collection('LanternUsers').updateOne({ 'auth.email': req.user.email }, { $push: {items: item, accessTokens: accessToken} }, (e, dbRes) => {
       if (e) {
         res.status(500).json({ message: 'Database insert Item Error!' });
       } else {
