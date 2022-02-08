c54ecd441afeccbf1dcf86c0eb9a7e224266404e
Shibi Balamurugan <shibibala1@gmail.com>
2022-02-08 16:33:01 -0500

    Add eslint Mocha env

--- users/usersController.js
+++ users/usersController.js
@@ -1,7 +1,7 @@
 const mongoDBConnection = require('../mongoDBConnection');
 const { generateAccessToken } = require('../helpers/jwt');
 const { hashPass, isValidPass } = require('../helpers/passwords');
-const { check, validationResult } = require("express-validator");
+const { check, validationResult } = require('express-validator');
 /*
 POST /register
 
@@ -23,21 +23,21 @@ Response:
 */
 module.exports.register = [
   // Perform input validation
-  check("bio.first").isLength({ min: 1 }).trim().withMessage("First name must be specified.")
-		.isAlphanumeric().withMessage("First name has non-alphanumeric characters."),
-  check("bio.last").isLength({ min: 1 }).trim().withMessage("Last name must be specified.")
-		.isAlphanumeric().withMessage("Last name has non-alphanumeric characters."),
-  check("bio.orgName").isLength({ min: 1 }).trim().withMessage("Last name must be specified.")
-		.isAlphanumeric().withMessage("Last name has non-alphanumeric characters."),
-	check("auth.email").isLength({ min: 1 }).trim().withMessage("Email must be specified.")
-		.isEmail().withMessage("Email must be a valid email address."),
-  check("auth.password").isLength({ min: 6 }).trim().withMessage("Password must be 6 characters or greater."),
+  check('bio.first').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  check('bio.last').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+    .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
+  check('bio.orgName').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+    .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
+  check('auth.email').isLength({ min: 1 }).trim().withMessage('Email must be specified.')
+    .isEmail().withMessage('Email must be a valid email address.'),
+  check('auth.password').isLength({ min: 6 }).trim().withMessage('Password must be 6 characters or greater.'),
   (req, res, next) => {
   // Validate unique email
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // Display sanitized values/errors messages.
-      return res.status(400).json({message:errors.array()[0].msg});
+      return res.status(400).json({ message: errors.array()[0].msg });
     } else {
       mongoDBConnection.get().collection('LanternUsers').find({ 'auth.email': req.body.auth.email }).toArray(async (e, docs) => {
         if (docs.length !== 0) {
@@ -111,31 +111,31 @@ Response:
 {message: "string"}
 */
 module.exports.updateUserProfile = [
-  check("req.first").isLength({ min: 1 }).trim().withMessage("First name must be specified.")
-		.isAlphanumeric().withMessage("First name has non-alphanumeric characters."),
-  check("last").isLength({ min: 1 }).trim().withMessage("Last name must be specified.")
-		.isAlphanumeric().withMessage("Last name has non-alphanumeric characters."),
-  check("orgName").isLength({ min: 1 }).trim().withMessage("Last name must be specified.")
-		.isAlphanumeric().withMessage("Last name has non-alphanumeric characters."),
+  check('req.first').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  check('last').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+    .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
+  check('orgName').isLength({ min: 1 }).trim().withMessage('Last name must be specified.')
+    .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
   (req, res) => {
-  mongoDBConnection.get().collection('LanternUsers').find({ 'auth.email': req.user.email }).toArray((e, docs) => {
-    if (docs.length === 0) {
-      return res.status(400).json({ message: 'Account invalid' });
-    } else {
+    mongoDBConnection.get().collection('LanternUsers').find({ 'auth.email': req.user.email }).toArray((e, docs) => {
+      if (docs.length === 0) {
+        return res.status(400).json({ message: 'Account invalid' });
+      } else {
       // Update document
-      const modifiedBio = {
-        'bio.first': req.body.first,
-        'bio.last': req.body.last,
-        'bio.orgName': req.body.orgName
-      };
+        const modifiedBio = {
+          'bio.first': req.body.first,
+          'bio.last': req.body.last,
+          'bio.orgName': req.body.orgName
+        };
 
-      mongoDBConnection.get().collection('LanternUsers').updateOne({ _id: docs[0]._id }, { $set: modifiedBio }, (e, dbRes) => {
-        if (e) {
-          return res.status(500).json({ message: 'Database Update Error' });
-        } else {
-          return res.status(200).json({ message: 'Database Updated' });
-        }
-      });
-    }
-  });
-}];
+        mongoDBConnection.get().collection('LanternUsers').updateOne({ _id: docs[0]._id }, { $set: modifiedBio }, (e, dbRes) => {
+          if (e) {
+            return res.status(500).json({ message: 'Database Update Error' });
+          } else {
+            return res.status(200).json({ message: 'Database Updated' });
+          }
+        });
+      }
+    });
+  }];
