ebf9b27d2b049b2ad38228183725cba7392f3fa1
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-02-08 01:06:26 -0500

    #20 add exchange handler

--- link/linkController.js
+++ link/linkController.js
@@ -29,3 +29,21 @@ module.exports.create = async (req, res) => {
     console.log(error);
   }
 };
+
+module.exports.exchange = async (req, res) => {
+  // Exchange public token for access token
+  let publicToken = req.body.token;
+  const request = {
+    public_token: publicToken
+  };
+  try {
+    const response = await plaid.client.itemPublicTokenExchange(request);
+    const accessToken = response.data.access_token;
+    const itemId = response.data.item_id;
+    // TODO Store item in database?
+    res.status(200).json({ token: accessToken });
+  } catch (error) {
+    // handle error
+    console.log(error)
+  }
+};
