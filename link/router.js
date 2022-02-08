4a5d1048a7dc893aa4e2c66ffe533c71fc169ee7
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-02-08 01:49:34 -0500

    #20 Complete exchange endpoint and tests

--- link/router.js
+++ link/router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const linkController = require('./linkController');
 
 router.get('/link', linkController.create);
+router.post('/link', linkController.exchange);
 
 module.exports = router;
--- test/linkTest.js
+++ test/linkTest.js
@@ -1,7 +1,8 @@
-const { chai, server, assert, should } = require('./testConfig');
+const {chai, server, assert, should} = require('./testConfig');
+const plaid = require("../plaidConnection");
 
 // Get Link Token test
-describe('/GET /api/link/create', () => {
+describe('/GET /api/link', () => {
     // Get a link token
     it('It should get a Plaid link token', (done) => {
         // First login and get token
@@ -21,4 +22,34 @@ describe('/GET /api/link/create', () => {
 });
 
 // Exchange public token for access token test
-// Insert here
+describe('/POST /api/link', () => {
+    let publicToken;
+    it('It should get a sandbox public token',(done) => {
+        const publicTokenRequest = {
+            institution_id: "ins_109508",
+            initial_products: [plaid.Products.Transactions, plaid.Products.Auth],
+        };
+        plaid.client.sandboxPublicTokenCreate(publicTokenRequest).then((result) => {
+            result.data.public_token.should.not.equal("");
+            publicToken = result.data.public_token;
+            done();
+        });
+    });
+
+    it('It should exchange public_token for access_token',(done) => {
+        // First login and get client token
+        const loginPayload = {email: 'test@gmail.com', password: 'pass'};
+        chai.request(server).post('/api/users/authenticate').send(loginPayload).end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property("token");
+            let clientToken = res.body.token;
+            // Now hit exchange payload
+            const exchangePayload = {token: publicToken};
+            chai.request(server).post('/api/link').set('Authorization', 'Bearer ' + clientToken).send(exchangePayload).end((err2, res2) => {
+                res.should.have.status(200);
+                res.body.should.have.property("token");
+                done();
+            });
+        });
+    });
+});
