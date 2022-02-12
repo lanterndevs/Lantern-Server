475dc9ee03a402ba6cd3be25b279beddf0c20f25
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2022-02-12 14:40:01 -0500

    #16 eslint fixes

--- test/transactionsTest.js
+++ test/transactionsTest.js
@@ -2,25 +2,25 @@ const { chai, server } = require('./testConfig');
 
 // Get transactions test
 describe('/GET /api/transactions', () => {
-    it('It should get all transactions for a given users accounts', (done) => {
-        // First login and get token
-        const loginPayload = { email: 'test@gmail.com', password: 'password' };
-        chai.request(server).post('/api/users/authenticate').send(loginPayload).end((err, res) => {
-            if (err) {
-                console.log(err);
-            }
-            res.should.have.status(200);
-            res.body.should.have.property('token');
-            const token = res.body.token;
-            // Now hit transactions endpoint using token
-            chai.request(server).get('/api/transactions').set('Authorization', 'Bearer ' + token).end((err2, res2) => {
-                if (err2) {
-                    console.log(err2);
-                }
-                res2.should.have.status(200);
-                res2.body.should.have.lengthOf.above(0);
-                done();
-            });
-        });
-    }).timeout(0);
+  it('It should get all transactions for a given users accounts', (done) => {
+    // First login and get token
+    const loginPayload = { email: 'test@gmail.com', password: 'password' };
+    chai.request(server).post('/api/users/authenticate').send(loginPayload).end((err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      res.should.have.status(200);
+      res.body.should.have.property('token');
+      const token = res.body.token;
+      // Now hit transactions endpoint using token
+      chai.request(server).get('/api/transactions').set('Authorization', 'Bearer ' + token).end((err2, res2) => {
+        if (err2) {
+          console.log(err2);
+        }
+        res2.should.have.status(200);
+        res2.body.should.have.lengthOf.above(0);
+        done();
+      });
+    });
+  }).timeout(0);
 });
--- transactions/transactionsController.js
+++ transactions/transactionsController.js
@@ -1,6 +1,5 @@
 const plaid = require('../plaidConnection');
-let dateHelper = require('../helpers/dates');
-const {copyDate, addYears, getFormattedDate} = require("../helpers/dates");
+const { copyDate, addYears, getFormattedDate } = require('../helpers/dates');
 
 /*
 GET /transactions
@@ -11,54 +10,54 @@ Response: Array of transactions
 */
 
 module.exports.getTransactions = async (req, res) => {
-    // Get transactions from plaid
-    const transactions = [];
-    let today = new Date();
-    let start = addYears(copyDate(today), -2);
-    let todayString = getFormattedDate(today);
-    let startString = getFormattedDate(start);
-    for (let i = 0; i < req.user.items.length; i++) {
-        const transactionsRequest = {
-            access_token: req.user.items[i].accessToken,
-            start_date: startString,
-            end_date: todayString
+  // Get transactions from plaid
+  const transactions = [];
+  const today = new Date();
+  const start = addYears(copyDate(today), -2);
+  const todayString = getFormattedDate(today);
+  const startString = getFormattedDate(start);
+  for (let i = 0; i < req.user.items.length; i++) {
+    const transactionsRequest = {
+      access_token: req.user.items[i].accessToken,
+      start_date: startString,
+      end_date: todayString
+    };
+    try {
+      const response = await plaid.client.transactionsGet(transactionsRequest);
+      let plaidTransactions = response.data.transactions;
+      const totalTransactions = response.data.total_transactions;
+      // Manipulate the offset parameter to paginate
+      // transactions and retrieve all available data
+      while (plaidTransactions.length < totalTransactions) {
+        const paginatedRequest = {
+          access_token: req.user.items[i].accessToken,
+          start_date: startString,
+          end_date: todayString,
+          options: {
+            offset: plaidTransactions.length
+          }
         };
-        try {
-            const response = await plaid.client.transactionsGet(transactionsRequest);
-            let plaidTransactions = response.data.transactions;
-            const total_transactions = response.data.total_transactions;
-            // Manipulate the offset parameter to paginate
-            // transactions and retrieve all available data
-            while (plaidTransactions.length < total_transactions) {
-                const paginatedRequest = {
-                    access_token: req.user.items[i].accessToken,
-                    start_date: startString,
-                    end_date: todayString,
-                    options: {
-                        offset: plaidTransactions.length,
-                    },
-                };
-                const paginatedResponse = await plaid.client.transactionsGet(paginatedRequest);
-                plaidTransactions = plaidTransactions.concat(
-                    paginatedResponse.data.transactions,
-                );
-            }
-            for (let j = 0; j < plaidTransactions.length; j++) {
-                transactions.push({
-                    accountID: plaidTransactions[j].account_id,
-                    amount: plaidTransactions[j].amount,
-                    date: plaidTransactions[j].date,
-                    categories: plaidTransactions[j].category,
-                    details: plaidTransactions[j].location.city + ", " + plaidTransactions[j].location.state + " - " + plaidTransactions[j].payment_channel.toUpperCase(),
-                    name: plaidTransactions[j].name
-                });
-            }
-        } catch (err) {
-            // handle error
-            console.log(err);
-        }
+        const paginatedResponse = await plaid.client.transactionsGet(paginatedRequest);
+        plaidTransactions = plaidTransactions.concat(
+          paginatedResponse.data.transactions
+        );
+      }
+      for (let j = 0; j < plaidTransactions.length; j++) {
+        transactions.push({
+          accountID: plaidTransactions[j].account_id,
+          amount: plaidTransactions[j].amount,
+          date: plaidTransactions[j].date,
+          categories: plaidTransactions[j].category,
+          details: plaidTransactions[j].location.city + ', ' + plaidTransactions[j].location.state + ' - ' + plaidTransactions[j].payment_channel.toUpperCase(),
+          name: plaidTransactions[j].name
+        });
+      }
+    } catch (err) {
+      // handle error
+      console.log(err);
     }
+  }
 
-    // Return transactions
-    res.status(200).json(transactions);
+  // Return transactions
+  res.status(200).json(transactions);
 };
